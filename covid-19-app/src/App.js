36835b001707ea9ba31ffb29387029946904e664
Hemant Govekar <hemantgovekar@gmail.com>
2020-05-30 14:43:30 +0530

    COmponent ready

--- covid-19-app/src/App.js
+++ covid-19-app/src/App.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 
- class App extends Component {
+import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css';
+
+import { fetchData } from './api';
+
+class App extends Component {
+
+    async  componentDidMount() {
+        const data = await fetchData();
+        console.log(data);
+    }
+
     render() {
         return (
-            <div>
-                <h1>App</h1>
+            <div className={styles.container}>
+                <Cards />
+                <Chart />
+                <CountryPicker />
             </div>
         )
     }
--- covid-19-app/src/index.js
+++ covid-19-app/src/index.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-
 
 import App from './App';
 
